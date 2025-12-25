198dea7f7d734672f5bd5f0326f9add2ca0a35c0
Leruva <sjruva2006@gmail.com>
2025-12-25 19:49:49 +0530

    Fix: setLoading

--- src/components/layout/landing/Upcoming.jsx
+++ src/components/layout/landing/Upcoming.jsx
@@ -23,7 +23,7 @@ import { getUpcomingEvents } from '@/firebase/collections'
 
 const Upcoming = () =>{
   const [events, setEvents] = useState([]);
-  const [loading, Fding] = useState(true);
+  const [loading, setLoading] = useState(true);
   useEffect(()=>{
     const fetchEvents = async ()=>{
       const data = await getUpcomingEvents();
